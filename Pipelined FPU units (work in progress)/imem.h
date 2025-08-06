e5e2378f123b70106f7ac8b69e3ad48b11702978
ashvar97 <113519947+ashvar97@users.noreply.github.com>
2025-08-06 12:42:37 +0200

    Create imem.h

---
+++ Pipelined FPU units (work in progress)/imem.h
@@ -0,0 +1,23 @@
+// Separate instruction memory module
+SC_MODULE(InstructionMemory) {
+    sc_in<sc_uint<32>> address;
+    sc_out<sc_uint<32>> instruction;
+    
+    // Instruction memory array
+    sc_signal<sc_uint<32>> imem[256];
+    
+    void process_read() {
+        // Read from instruction memory based on address (word-aligned)
+        instruction.write(imem[address.read().range(9,2)].read());
+    }
+    
+    // No initialization - will be done by testbench
+    
+    SC_CTOR(InstructionMemory) {
+        SC_METHOD(process_read);
+        sensitive << address;
+        for (int i = 0; i < 256; i++) {
+            sensitive << imem[i];
+        }
+    }
+};
