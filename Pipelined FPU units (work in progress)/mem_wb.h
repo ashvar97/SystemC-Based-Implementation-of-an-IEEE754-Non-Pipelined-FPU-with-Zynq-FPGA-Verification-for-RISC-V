0e33510aeb505ea1d2c77aa1b68cf6dfe06fa928
ashvar97 <113519947+ashvar97@users.noreply.github.com>
2025-08-14 22:42:58 +0200

    Update mem_wb.h

--- Pipelined FPU units (work in progress)/mem_wb.h
+++ Pipelined FPU units (work in progress)/mem_wb.h
@@ -1,5 +1,6 @@
 
 SC_MODULE(Memory) {
+    sc_in<bool> clk;
     sc_in<bool> reset;
     sc_in<bool> stall;
     sc_in<bool> valid_in;
@@ -7,13 +8,13 @@ SC_MODULE(Memory) {
     sc_in<sc_uint<5>> rd_in;
     sc_in<bool> reg_write_in;
     sc_in<sc_uint<32>> instruction_in;
-
+    
     sc_out<sc_uint<32>> result_out;
     sc_out<sc_uint<5>> rd_out;
     sc_out<bool> reg_write_out;
     sc_out<bool> valid_out;
     sc_out<sc_uint<32>> instruction_out;
-
+    
     void memory_process() {
         // Initialize outputs
         result_out.write(0);
@@ -21,28 +22,41 @@ SC_MODULE(Memory) {
         reg_write_out.write(false);
         valid_out.write(false);
         instruction_out.write(0);
-
-        if (!reset.read() && !stall.read()) {
-            result_out.write(result_in.read());
-            rd_out.write(rd_in.read());
-            reg_write_out.write(reg_write_in.read());
-            valid_out.write(valid_in.read());
-            instruction_out.write(instruction_in.read());
-
-            if (valid_in.read()) {
-                sc_uint<32> opcode = (instruction_in.read() >> 25) & 0x7F;
-                cout << "MEM @" << sc_time_stamp() << ": ";
-                cout << "rd=f" << rd_in.read();
-                cout << " opcode=0x" << opcode << endl;
+        
+        wait();
+        
+        while (true) {
+            if (reset.read()) {
+                result_out.write(0);
+                rd_out.write(0);
+                reg_write_out.write(false);
+                valid_out.write(false);
+                instruction_out.write(0);
+            } else if (!stall.read()) {
+                // FIXED: Simple pass-through - no modifications
+                result_out.write(result_in.read());
+                rd_out.write(rd_in.read());
+                reg_write_out.write(reg_write_in.read());
+                valid_out.write(valid_in.read());
+                instruction_out.write(instruction_in.read());
+                
+                if (valid_in.read() && reg_write_in.read()) {
+                    sc_uint<32> instruction = instruction_in.read();
+                    sc_uint<7> funct7 = instruction.range(31, 25);
+                    cout << "MEM @" << sc_time_stamp() << ": ";
+                    cout << "rd=f" << rd_in.read();
+                    cout << " funct7=0x" << hex << funct7 << dec;
+                    cout << " result=0x" << hex << result_in.read() << dec << endl;
+                }
             }
+            wait();
         }
     }
-
+    
     SC_CTOR(Memory) {
-        SC_METHOD(memory_process);
-        sensitive << reset << stall << valid_in << result_in << rd_in
-                 << reg_write_in << instruction_in;
-        // Initialize outputs
+        SC_CTHREAD(memory_process, clk.pos());
+        reset_signal_is(reset, true);
+        
         result_out.initialize(0);
         rd_out.initialize(0);
         reg_write_out.initialize(false);
@@ -51,7 +65,7 @@ SC_MODULE(Memory) {
     }
 };
 
-// Clean Writeback Module - Debug portions removed
+
 SC_MODULE(Writeback) {
     sc_in<bool> clk;
     sc_in<bool> reset;
